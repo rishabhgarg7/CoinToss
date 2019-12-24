9d6e504fe6e00594536414faf591ff70bf1aafb6
Rishabh Garg <rishabhgrg77@gmail.com>
2019-12-24 17:15:46 +0530

    Completed

--- src/components/TossCoin.jsx
+++ src/components/TossCoin.jsx
@@ -6,7 +6,7 @@ import Stats from "./Stats";
 class TossCoin extends Component {
   constructor(props) {
     super(props);
-    this.state = { ntotal: 0, nheads: 0 };
+    this.state = { ntotal: 0, nheads: 0, outcome: 0 };
     this.handleToss = this.handleToss.bind(this);
   }
 
@@ -15,22 +15,23 @@ class TossCoin extends Component {
     let rn = Math.random() > 0.5 ? 1 : 0;
     this.setState({
       ntotal: this.state.ntotal + 1,
-      nheads: this.state.nheads + rn
+      nheads: this.state.nheads + rn,
+      outcome: rn
     });
   }
 
   render() {
+    let out = this.state.outcome === 1 ? "heads" : "tails";
+    let imUrl = "https://tinyurl.com/react-coin-" + out + "-jpg";
+    console.log(imUrl);
     return (
       <div>
         <h2 style={{ alignContent: "center" }}>Let's flip a coin</h2>
+
         {this.state.ntotal == 0 ? (
           ""
         ) : (
-          <img
-            src="https://tinyurl.com/react-coin-heads-jpg"
-            width="150px"
-            height="150px"
-          />
+          <img src={imUrl} width="150px" height="150px" />
         )}
         <br />
         <button onClick={this.handleToss}>Flip Mee!</button>
